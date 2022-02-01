cfbfcb972dff41d016673a8e61676ac9423bb9f7
Sam Bencivengo <sambencivengo@gmail.com>
2022-02-01 17:22:17 -0500

    testing server before database connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,25 @@
 const express = require('express');
-
 const app = express();
 const cors = require('cors');
-require('dotenv');
-
+const connectDB = require('./db/connect');
+require('dotenv').config({ path: './.env' });
 const port = process.env.PORT || 5000;
+const db = process.env.MONGO_URI;
+app.use(cors());
+app.use(express.json());
+// app.use(require('./routes/record'));
+
+// app.use('/api/v1/gps');
+
+const start = async () => {
+	try {
+		await connectDB(db);
+		app.listen(port, () => {
+			console.log(`server is listening on port ${port}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
