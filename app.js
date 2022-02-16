b89200aad758c7f96c74540f19ff891971a7a77b
Sam Bencivengo <sambencivengo@gmail.com>
2022-02-16 11:00:03 -0500

    node cookie package expirementation

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const port = process.env.PORT || 5000;
 const db = process.env.MONGO_URI;
 const ridesRouter = require('./routes/ridesRouter');
 const userRouter = require('./routes/userRouter');
+const cookieParser = require('cookie-parser');
+
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 // app.use(require('./routes/record'));
 app.use(function (req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -24,6 +27,21 @@ app.use('/user/portal', userRouter);
 
 app.use('/api/v1/rides', ridesRouter);
 
+// COOKIES
+app.get('/set-cookies', (req, res) => {
+	res.cookie('newUser', false);
+	res.cookie('employee', true, {
+		maxAge: 1000 * 60 * 60 * 24,
+		httpOnly: true,
+	});
+	res.send('you got cookies!');
+});
+
+app.get('/read-cookies', (req, res) => {
+	const cookies = req.cookies;
+	console.log(cookies);
+	res.json(cookies.newUser);
+});
 //
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.get('*', (req, res) => {
