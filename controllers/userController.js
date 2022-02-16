4df22fe04152038fd719303fde2493858427c1d2
Sam Bencivengo <sambencivengo@gmail.com>
2022-02-15 21:54:46 -0500

    reformatted the user model. Added some validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,41 +2,64 @@ const bcrypt = require('bcryptjs/dist/bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const registerUser = async (req, res) => {
-	try {
-		const { userName, email, password } = req.body;
+const handleErrors = (err) => {
+	console.log(err.message, err.code);
+	const errors = {
+		email: '',
+		password: '',
+	};
 
-		// make sure the inputs are filled out
-		if (!(email && userName && password)) {
-			res.status(400).json({
-				success: false,
-				msg: 'All input is required',
-			});
-		}
-
-		// check for the existing user
-		const existingUser = await User.findOne({ email });
-		if (existingUser) {
-			return res.status(409).json({
-				success: false,
-				msg: 'An user with this email already exists',
-			});
-		}
-
-		const encryptedPassword = await bcrypt.hash(password, 10);
-
-		// create user in the DB
-		const user = await User.create({
-			userName,
-			email: email.toLowerCase(),
-			password: encryptedPassword,
+	//validation errors
+	if (err.message.includes('user validation failed')) {
+		Object.values(err.errors).forEach(({ properties }) => {
+			errors[properties.path] = properties.message;
 		});
-
-		//send response
-		res.status(201).json({ user });
-	} catch (error) {
-		console.log(error);
 	}
+	return errors;
+};
+
+const registerUser = async (req, res) => {
+	const { email, username, password } = req.body;
+	console.log(email, username, password);
+
+	try {
+		const user = await User.create({ email, username, password });
+		res.status(201).json(user);
+	} catch (error) {
+		const errors = handleErrors(error);
+		res.status(400).json({ errors });
+	}
+	// OLD BLOCK
+	//
+	// try {
+	// 	const { userName, email, password } = req.body;
+	// 	// make sure the inputs are filled out
+	// 	if (!(email && userName && password)) {
+	// 		res.status(400).json({
+	// 			success: false,
+	// 			msg: 'All input is required',
+	// 		});
+	// 	}
+	// 	// check for the existing user
+	// 	const existingUser = await User.findOne({ email });
+	// 	if (existingUser) {
+	// 		return res.status(409).json({
+	// 			success: false,
+	// 			msg: 'An user with this email already exists',
+	// 		});
+	// 	}
+	// 	const encryptedPassword = await bcrypt.hash(password, 10);
+	// 	// create user in the DB
+	// 	const user = await User.create({
+	// 		userName,
+	// 		email: email.toLowerCase(),
+	// 		password: encryptedPassword,
+	// 	});
+	// 	//send response
+	// 	res.status(201).json({ user });
+	// } catch (error) {
+	// 	console.log(error);
+	// }
 };
 
 const userLogin = async (req, res) => {
@@ -58,7 +81,6 @@ const userLogin = async (req, res) => {
 					expiresIn: '30d',
 				}
 			);
-			
 
 			// saves the token
 			res.status(200).json({ user });
--- models/user.js
+++ models/user.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-	userName: { type: String, required: [true, 'Please enter a username'] },
+	username: {
+		type: String,
+		required: [true, 'Please enter a username'],
+		unique: [true, 'This username has already been taken'],
+		lowercase: true,
+	},
 	email: {
 		type: String,
 		required: [true, 'Please enter a valid email address'],
+		unique: [true, 'An account associated with this email already exists'],
+		lowercase: true,
+		validate: [validator.isEmail, 'Please enter a valid email'],
 	},
 	password: {
 		type: String,
-		required: [true, 'Please enter a valid password'],
+		required: [true, 'Please enter a password'],
+		minlength: [
+			6,
+			'Please enter a password that is at least 6 characters in length',
+		],
 	},
 	token: { type: String },
 });
